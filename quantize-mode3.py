1056d8d34b33e39ff3b84945f962215100e0d84d
Mike <barbeque@users.noreply.github.com>
2024-08-22 16:14:30 -0600

    Fix pack_into_byte with tests

--- quantize-mode3.py
+++ quantize-mode3.py
@@ -16,3 +16,39 @@ palette.putpalette(MODE_3_PALETTE)
 oldimage = Image.open('meules.png').convert('RGB').transpose(Image.ROTATE_270).resize(MODE_3_SIZE)
 newimage = oldimage.quantize(4, palette=palette)
 newimage.save('meules-4.png')
+
+def pack_into_byte(a):
+    assert(len(a) == 4)
+    tb = 0
+    for b in range(4):
+        assert(a[b] >= 0)
+        assert(a[b] < 4)
+        tb = (tb << 2) | (a[b])
+        print(bin(tb))
+    return tb
+
+assert(pack_into_byte([0,0,0,0]) == 0)
+assert(pack_into_byte([3,3,3,3]) == 0xff)
+assert(pack_into_byte([0,1,2,3]) == 0b00011011)
+
+# go through and generate the asm now
+buf = ''
+pixels = newimage.load()
+for y in range(MODE_3_SIZE[1]):
+    buf += '.db '
+
+    x = 0
+    bytes_buf = []
+    while x < MODE_3_SIZE[0]:
+        # pack into 1-byte chunks (2-bits each)
+        tb = pack_into_byte([
+            pixels[x, y],
+            pixels[x + 1, y],
+            pixels[x + 2, y],
+            pixels[x + 3, y],
+        ])
+        bytes_buf.append(tb)
+        x += 4
+    buf += ', '.join([str(b) for b in bytes_buf]) + '\n'
+
+print(buf)
