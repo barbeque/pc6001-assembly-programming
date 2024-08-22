20302ff958a94054c2164351acf888f9d65ca9c5
Mike <barbeque@users.noreply.github.com>
2024-08-22 15:55:00 -0600

    Tate mode

--- quantize-mode3.py
+++ quantize-mode3.py
@@ -6,11 +6,13 @@ MODE_3_PALETTE = [
         255, 0, 0
         ]
 
+MODE_3_SIZE = (128, 192)
+
 from PIL import Image, ImagePalette
 
 palette = Image.new('P', (4,1))
 palette.putpalette(MODE_3_PALETTE)
 
-oldimage = Image.open('meules.png').convert('RGB')
+oldimage = Image.open('meules.png').convert('RGB').transpose(Image.ROTATE_270).resize(MODE_3_SIZE)
 newimage = oldimage.quantize(4, palette=palette)
 newimage.save('meules-4.png')
